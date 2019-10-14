972ec915c35d3b5ad64c5e67660da71e7d497e73
Arturo Carcamo <34602460+Arturo7C@users.noreply.github.com>
2019-10-14 16:46:02 -0400

    Initial setup for the CNN
    Initial setup for basic dependencies. not finished yet.

--- ImageRecognition/CNN/ConvolutionalNeuralNetwork.py
+++ ImageRecognition/CNN/ConvolutionalNeuralNetwork.py
@@ -5,3 +5,24 @@ Created on Mon Oct 14 15:54:53 2019
 @author: Arturo
 """
 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+
+classifier = Sequential()
+
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relus'))
+
+classifer.add(MaxPooling2D(pool_size=(2, 2)))
+
+classifer.add(Convolution2D(32, 32, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+
+#flattening
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+classifier.compile(optimizer = 'data', loss = 'binary_crossentropy', metrics = ['accuracy'])
